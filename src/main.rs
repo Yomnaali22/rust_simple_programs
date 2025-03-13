0181bd3280ee589374a80780fe74061f69995bc5
Yomnaali22 <56122735+Yomnaali22@users.noreply.github.com>
2025-03-13 22:38:05 +0200

    build custom filtering function and write the closure option too

--- src/main.rs
+++ src/main.rs
@@ -1,61 +1,29 @@
-use rand::Rng; // import the Rng trait from the rand crate to generate random numbers
-mod account;
-mod save_data; // declares the module
-use crate::account::{Account, BankAccount};
-// use account::{Account, BankAccount}; // brings the BankAccount struct and Account trait into scope
-use save_data::add_account; // brings function into scope
+struct FilterCondition {
+    item: i32,
+}
+
+impl FilterCondition {
+    fn is_match(&self, item: &i32) -> bool {
+        item == &self.item
+    }
+}
+fn custom_filter(collection: &Vec<i32>, filter_struct: &FilterCondition) -> Vec<i32> {
+    let mut filtered_collection: Vec<i32> = Vec::new();
+    for item in collection.iter() {
+        if filter_struct.is_match(&item) {
+            filtered_collection.push(*item); //dereference pushed values 
+        }
+    }
+    filtered_collection
+}
 
 fn main() {
-    // Generates a random number between 1 and 100
-    let mut rng = rand::thread_rng();
-    let account_number: u32 = rng.gen_range(1..101);
-
-    // prompt the user to a holder name;
-    let mut holder_name: String = String::new();
-    println!("if want to open a bank account please enter your name: ");
-    std::io::stdin().read_line(&mut holder_name).unwrap();
-    let mut account = BankAccount {
-        account_number,
-        holder_name,
-        balance: 0.0,
-    };
-
-    println!("hello {} ", account.holder_name);
-    print!("Account number: {} ", account.account_number);
-    add_account({
-        BankAccount {
-            account_number,
-            holder_name: account.holder_name.clone(),
-            balance: 0.0,
-        }
-    });
-
-    // ask if user wants to deposit or withdraw
-    let mut choice = String::new();
-    println!("Do you want to deposit, withdraw or see current balance? ");
-    std::io::stdin().read_line(&mut choice).unwrap();
-    let choice = choice.trim();
-    let mut amount = String::new();
-
-    match choice {
-        "deposit" => {
-            println!("Enter the amount: ");
-            std::io::stdin().read_line(&mut amount).unwrap();
-            // check if the user enters a valid amount
-            match amount.trim().parse::<f64>() {
-                Ok(amount) => account.deposit(amount),
-                Err(_) => println!("Invalid amount entered"),
-            }
-        }
-        "withdraw" => {
-            println!("Enter the amount: ");
-            std::io::stdin().read_line(&mut amount).unwrap();
-            match amount.trim().parse::<f64>() {
-                Ok(amount) => account.withdraw(amount),
-                Err(_) => println!("Invalid amount entered"),
-            }
-        }
-        "balance" => account.balance(),
-        _ => println!("Invalid choice"),
-    }
+    let filter_struct = FilterCondition { item: 4 };
+    let collection = vec![1, 2, 3, 4, 5];
+    // without closures
+    let filtered = custom_filter(&collection, &filter_struct);
+    println!("{:?}", filtered);
+    // using closures
+    let filtered: Vec<i32> = collection.into_iter().filter(|x| *x == 4).collect(); // dereference x 
+    println!("{:?}", filtered);
 }
